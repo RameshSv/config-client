881c3c41204cae7b6f5b1b107ef6ee159c37149d
Ramesh <rameshredddy.salla@gmail.com>
2024-02-11 23:58:48 +0530

    Initial commit

---
+++ src/main/java/com/spring/io/configclient/ConfigClientApplication.java
@@ -0,0 +1,27 @@
+package com.spring.io.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+@RefreshScope
+public class ConfigClientApplication {
+
+	@Value("${message}")
+	private String message;
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+
+	@PostMapping("/hello")
+	public String sayHello(@RequestBody String str){
+		System.out.println(str);
+		System.out.printf("hey"+message);
+		return message;
+	}
+}
