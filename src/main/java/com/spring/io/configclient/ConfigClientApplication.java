a2a479658537599af92f61e8d19addcfad4f8554
Ramesh <rameshredddy.salla@gmail.com>
2024-02-21 14:28:42 +0530

    update changes

--- src/main/java/com/spring/io/configclient/ConfigClientApplication.java
+++ src/main/java/com/spring/io/configclient/ConfigClientApplication.java
@@ -3,12 +3,14 @@ package com.spring.io.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
 @RefreshScope
+@EnableDiscoveryClient
 public class ConfigClientApplication {
 
 	@Value("${message}")
